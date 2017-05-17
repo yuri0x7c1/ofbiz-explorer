fa82d79f628ec9e45389b2a8e3f9a167e0fd1b8a
Yuri Rysich <yuri0x7c1@gmail.com>
2017-05-17 22:43:09 +0300

    Use vaadin icon instead font awesome icon for home page

--- src/main/java/com/github/yuri0x7c1/ofbiz/explorer/common/ui/view/HomeView.java
+++ src/main/java/com/github/yuri0x7c1/ofbiz/explorer/common/ui/view/HomeView.java
@@ -1,19 +1,19 @@
 package com.github.yuri0x7c1.ofbiz.explorer.common.ui.view;
 
-import org.vaadin.spring.sidebar.annotation.FontAwesomeIcon;
 import org.vaadin.spring.sidebar.annotation.SideBarItem;
+import org.vaadin.spring.sidebar.annotation.VaadinFontIcon;
 
 import com.github.yuri0x7c1.ofbiz.explorer.common.ui.sidebar.Sections;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.server.FontAwesome;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.VerticalLayout;
 
 
 @SpringView(name = "")
 @SideBarItem(sectionId = Sections.VIEWS, caption = "Home", order = 0)
-@FontAwesomeIcon(FontAwesome.HOME)
+@VaadinFontIcon(VaadinIcons.HOME)
 public class HomeView extends VerticalLayout implements View {
 
     public HomeView() {
