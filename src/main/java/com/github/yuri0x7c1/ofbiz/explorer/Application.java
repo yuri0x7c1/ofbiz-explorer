37f7a55399bd8eb83793478b5286a6032d61e462
Yuri Rysich <yuri0x7c1@gmail.com>
2017-07-17 17:37:13 +0300

    Upd

--- src/main/java/com/github/yuri0x7c1/ofbiz/explorer/Application.java
+++ src/main/java/com/github/yuri0x7c1/ofbiz/explorer/Application.java
@@ -1,16 +1,32 @@
 package com.github.yuri0x7c1.ofbiz.explorer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+
+import com.github.yuri0x7c1.ofbiz.explorer.entity.xml.Entity;
+import com.github.yuri0x7c1.ofbiz.explorer.generator.util.JpaEntityGenerator;
+import com.github.yuri0x7c1.ofbiz.explorer.util.OfbizInstance;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class Application {
+public class Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	OfbizInstance ofbizInstance;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+			JpaEntityGenerator g = new JpaEntityGenerator(ofbizInstance, "BudgetItem");
+			log.debug(g.generate());
 	}
 }
--- src/main/java/com/github/yuri0x7c1/ofbiz/explorer/generator/util/JpaEntityGenerator.java
+++ src/main/java/com/github/yuri0x7c1/ofbiz/explorer/generator/util/JpaEntityGenerator.java
@@ -1,9 +1,75 @@
 package com.github.yuri0x7c1.ofbiz.explorer.generator.util;
 
-public class JpaEntityGenerator extends EntityGenerator {
-	@Override
-	String generate() {
-		// TODO Auto-generated method stub
-		return null;
+import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+
+import com.github.yuri0x7c1.ofbiz.explorer.entity.xml.Entity;
+import com.github.yuri0x7c1.ofbiz.explorer.entity.xml.Field;
+import com.github.yuri0x7c1.ofbiz.explorer.util.OfbizInstance;
+
+import lombok.Getter;
+
+public class JpaEntityGenerator {
+	@Getter
+	private OfbizInstance ofbizInstance;
+
+	@Getter
+	private String entityName;
+
+	@Getter
+	private Entity entity;
+
+	public JpaEntityGenerator(OfbizInstance ofbizInstance, String entityName) {
+		super();
+		this.ofbizInstance = ofbizInstance;
+		this.entityName = entityName;
+
+		this.entity = ofbizInstance.getAllEntities().get(entityName);
 	}
+
+	public String generate() {
+		final JavaClassSource jpaEntityClass = Roaster.create(JavaClassSource.class);
+		jpaEntityClass.setPackage(entity.getPackageName())
+			.setName(entity.getEntityName());
+
+		for (Field field : entity.getField()) {
+			jpaEntityClass.addField()
+				.setName(field.getName())
+				.setType(getJavaType(field.getType()))
+				.setPrivate();
+		}
+
+		return jpaEntityClass.toString();
+	}
+
+	private String getJavaType(String type) {
+		if ("blob".equals(type)) {
+			return "java.sql.Blob";
+		} else if ("object".equals(type)) {
+			return "Object";
+		} else if ("byte-array".equals(type)) {
+			return "byte[]";
+
+		} else if ("currency-precise".equals(type)) {
+			return "BigDecimal";
+		} else if ("currency-amount".equals(type)) {
+			return "BigDecimal";
+		} else if ("fixed-point".equals(type)) {
+			return "BigDecimal";
+
+		} else if ("date".equals(type)) {
+			return "Date";
+		} else if ("time".equals(type)) {
+			return "Time";
+		} else if ("date-time".equals(type)) {
+			return "Date";
+
+		} else if ("numeric".equals(type)) {
+			return "Long";
+		} else if ("floating-point".equals(type)) {
+			return "Double";
+		}
+
+		return "String";
+}
 }
