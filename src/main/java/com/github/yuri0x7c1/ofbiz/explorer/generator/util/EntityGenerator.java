d33fc8b3a5772d828b75f681f36fd2fdf67f5ecc
Yuri Rysich <yuri0x7c1@gmail.com>
2017-11-19 21:36:09 +0200

    Generate fromValues method

--- src/main/java/com/github/yuri0x7c1/ofbiz/explorer/generator/util/EntityGenerator.java
+++ src/main/java/com/github/yuri0x7c1/ofbiz/explorer/generator/util/EntityGenerator.java
@@ -2,25 +2,14 @@ package com.github.yuri0x7c1.ofbiz.explorer.generator.util;
 
 import java.io.File;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jboss.forge.roaster.Roaster;
-import org.jboss.forge.roaster.model.source.AnnotationSource;
 import org.jboss.forge.roaster.model.source.FieldSource;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 import org.jboss.forge.roaster.model.source.MethodSource;
@@ -30,11 +19,9 @@ import org.springframework.stereotype.Component;
 
 import com.github.yuri0x7c1.ofbiz.explorer.entity.xml.Entity;
 import com.github.yuri0x7c1.ofbiz.explorer.entity.xml.Field;
-import com.github.yuri0x7c1.ofbiz.explorer.entity.xml.KeyMap;
 import com.github.yuri0x7c1.ofbiz.explorer.entity.xml.Relation;
 import com.github.yuri0x7c1.ofbiz.explorer.util.OfbizInstance;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -152,13 +139,12 @@ public class EntityGenerator {
 	}
 
 	/**
-	 * Create from value static method
+	 * Create fromValue static method
 	 * @param entity
 	 * @param entityClass
 	 */
 	private void createFromValueStaticMethod(Entity entity, JavaClassSource entityClass) {
 		String fromValueMethodBody = "return new " + entityClass.getName() + "(value);";
-
 
 		MethodSource<JavaClassSource> fromValueMethod = entityClass.addMethod()
 			.setName("fromValue")
@@ -168,6 +154,31 @@ public class EntityGenerator {
 			.setBody(fromValueMethodBody);
 
 		fromValueMethod.addParameter("org.ofbiz.entity.GenericValue", "value");
+	}
+
+	/**
+	 * Create fromValues static method
+	 * @param entity
+	 * @param entityClass
+	 */
+	private void createFromValuesStaticMethod(Entity entity, JavaClassSource entityClass) {
+		entityClass.addImport(List.class);
+		entityClass.addImport(ArrayList.class);
+
+		String fromValuesMethodBody = String.format("List<%s> entities = new ArrayList<>();"
+				+ "for (GenericValue value : values) {"
+				+ "		entities.add(new %s(value));"
+				+ "}"
+				+ "return entities;", entityClass.getName(), entityClass.getName());
+
+		MethodSource<JavaClassSource> fromValuesMethod = entityClass.addMethod()
+			.setName("fromValues")
+			.setPublic()
+			.setStatic(true)
+			.setReturnType("List<" + entityClass.getName() + ">")
+			.setBody(fromValuesMethodBody);
+
+			fromValuesMethod.addParameter("List<GenericValue>", "values");
 	}
 
 	/**
@@ -185,29 +196,11 @@ public class EntityGenerator {
 		// create constructor
 		createConstructorWithGenericValueParameter(entity, entityClass);
 
-		// create from value static method
+		// create fromValue static method
 		createFromValueStaticMethod(entity, entityClass);
 
-		/*
-		// from map method body
-		StringBuilder fromValueBody = new StringBuilder();
-
-		// create columns
-		for (Field field : entity.getField()) {
-			// append param to "fromValue()" body
-			fromValueBody.append(String.format("%s = %s map.get(\"%s\");",
-				field.getName(),
-				"(" + field.getJavaType().getName() + ")",
-				field.getName()));
-		}
-
-		// add "fromMap()" method to nested "In" type
-		MethodSource<JavaClassSource> fromValueSource = entityClass.addMethod()
-			.setName("fromValue")
-			.setPublic()
-			.setBody(fromValueBody.toString());
-		fromValueSource.addParameter("org.ofbiz.entity.GenericValue", "value");
-		*/
+		// create fromValues static method
+		createFromValuesStaticMethod(entity, entityClass);
 
 		String destinationPath = env.getProperty("generator.destination_path");
 
